12cb7eb7e318c2c119e5d02b066154d4cb282e4f
秦振坤 <johnqin@rektec.com.cn>
2019-09-03 21:44:48 +0800

    添加随机字母的方法

--- utils/util.js
+++ utils/util.js
@@ -104,6 +104,51 @@ function random(lower, upper) {
   return Math.random() * (upper - lower) + lower
 }
 
+// 随机一个大写字母
+export function randomUpperChar () {
+  return String.fromCharCode(random(65, 90))
+}
+
+// 随机一个小写写字母
+export function randomLowerChar () {
+  return String.fromCharCode(random(97, 122))
+}
+
+/**
+ * 获取指定字母表
+ * @param {*} type 0: 大小写字母表 1: 大写字母表 2: 小写字母表
+ */
+export function getCharList (type) {
+  let list = []
+  switch (type) {
+    case 0:
+      let arr1 = setCharList(65, 90)
+      let arr2 = setCharList(97, 122)
+      list = [...arr1, ...arr2]
+      break
+    case 1:
+      list = setCharList(65, 90)
+      break
+    default:
+      list = setCharList(97, 122)
+  }
+  return list
+}
+
+/**
+ * 生成指定位置的字母表
+ * @param {*} start
+ * @param {*} end
+ * return array 字母表
+ */
+export function setCharList (start, end) {
+  let list = []
+  for (let i = start; i <= end; i++) {
+    list.push(String.fromCharCode(i))
+  }
+  return list
+}
+
 /**
  * 金钱格式化  1234567, 1,234,567
  * @param {*} str 
