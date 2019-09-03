3bdf561b0862193ca86e54e87ec3428f763ac6f0
秦振坤 <johnqin@rektec.com.cn>
2019-09-03 21:42:10 +0800

    33

--- utils/string.js
+++ utils/string.js
@@ -48,3 +48,35 @@ function repeat(str, n) {
   }
   return res;
 }
+
+/*
+ * 检测密码强度
+*/
+function checkPwd (str) {
+  var Lv = 0;
+  if (str.length < 6) {
+      return Lv
+  }
+  if (/[0-9]/.test(str)) {
+      Lv++
+  }
+  if (/[a-z]/.test(str)) {
+      Lv++
+  }
+  if (/[A-Z]/.test(str)) {
+      Lv++
+  }
+  if (/[\.|-|_]/.test(str)) {
+      Lv++
+  }
+  return Lv;
+}
+
+/*过滤html代码(把<>转换)*/
+function filterTag (str) {
+  str = str.replace(/&/ig, "&amp;");
+  str = str.replace(/</ig, "&lt;");
+  str = str.replace(/>/ig, "&gt;");
+  str = str.replace(" ", "&nbsp;");
+  return str;
+}
