ab2c8cce65f2aa6ef1c3ddb5b13414763695fe60
秦振坤 <johnqin@rektec.com.cn>
2020-01-13 14:57:02 +0800

    node->处理post数据

---
+++ node/server.js
@@ -0,0 +1,34 @@
+/**
+ * post提交示例
+ */
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+
+const server = http.createServer((req, res) => {
+  let { pathname, query } = url.parse(req.url);
+  query = querystring.parse(query);
+  if (pathname === '/post') {
+    let str = '';
+    req.on('data', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.end(err);
+      }
+      str += data;
+    });
+
+    req.on('end', (err) => {
+      if (err) {
+        console.log(err);
+        res.end(err);
+      }
+      console.log(str);
+      res.end(str);
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log('http://localhost:3000已开启');
+})
